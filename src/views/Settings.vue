f4cda7b968410e5d1cbf1ae364291fb89747198e
Giulio Moretto - Promocare <giulio@promocare.it>
2021-03-04 15:48:48 +0100

    Feed color editing
    Feed color is now editable in Settings view

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -8,6 +8,8 @@
         Create Feed
       </button>
 
+      <Message v-for="message in messages" :color="message.color" class="mt-2" :key="message.id" :title="message.title" :message="message.message" @removeMessage="dismissMessage(message)" :duration="5000"/>
+
       <article
         v-for="feed in publicFeeds"
         :key="feed.url"
@@ -20,15 +22,18 @@
           >{{ feed.name }}
         </span>
 
-        <button
-          class="btn btn-orange text-white"
-          @click="
-            feedToDelete = feed.url;
-            showDeleteModal = true;
-          "
-        >
-          <img src="@/assets/img/x.svg" />
-        </button>
+        <div class="flex flex-row">
+          <button class="btn btn-primary mr-2" @click="doShowEditModal(feed)">Edit</button>
+          <button
+            class="btn btn-red text-white"
+            @click="
+              feedToDelete = feed.url;
+              showDeleteModal = true;
+            "
+          >
+            Delete
+          </button>
+        </div>
       </article>
     </main>
 
@@ -102,6 +107,41 @@
         </div>
       </template>
     </Modal>
+
+    <Modal title="Edit Feed" @close="hideEditModal()" v-show="showEditModal">
+      <template v-slot:body>
+        <div class="flex flex-row justify-around items-end">
+          <!-- <FormGroup label="Feed Name" type="text" v-model="selectedFeed.name" /> -->
+          <FormGroup
+            type="color"
+            label="color"
+            v-model="selectedFeed.color"
+            class="mx-2"
+          />
+        </div>
+      </template>
+      <template v-slot:footer>
+       <div class="flex flex-row justify-center items-center">
+          <button class="btn btn-gray mr-3" @click="hideEditModal()" :disabled="isSaving">
+            Cancel
+          </button>
+          <button class="btn btn-primary" @click="editFeed()" :disabled="isSaving">
+            <img
+              src="@/assets/img/loader.svg"
+              class="animate-spin mr-2"
+              style="
+                line {
+                  stroke: white;
+                }
+              "
+              v-if="isSaving"
+            />
+            {{ isSaving ? "Saving" : "Save" }}
+          </button>
+        </div>
+      </template>
+    </Modal>
+
   </div>
 </template>
 
@@ -109,6 +149,7 @@
 import Sidebar from "../components/Sidebar";
 import Modal from "../components/Modal";
 import FormGroup from "../components/FormGroup";
+import Message from '../components/Message';
 
 import Vue from "vue";
 
@@ -117,15 +158,19 @@ export default {
     Sidebar,
     Modal,
     FormGroup,
+    Message,
   },
   data: () => ({
     newFeedName: "",
     newFeedColor: "#000000",
     showNewFeedModal: false,
     showDeleteModal: false,
+    showEditModal:false,
     feedToDelete: "",
     isSaving:false,
     isDeleting:false,
+    selectedFeed:{name:"",color:"#000000"},
+    messages:[],
   }),
   computed: {
     ibex() {
@@ -183,6 +228,56 @@ export default {
           alert("Error deleting feed");
           this.showDeleteModal = false;
         });
+    },
+    doShowEditModal(feed){
+      console.log(feed);
+      this.showEditModal = true;
+      this.selectedFeed = feed;
+    },
+    hideEditModal(){
+      this.showEditModal = false;
+      this.selectedFeed = {name:"",color:"#000000"}
+    },
+    editFeed(){
+      this.isSaving = true;
+      this.ibex.manifest()
+      .then(manifest=>{
+        if(typeof manifest[this.selectedFeed.url] !== 'undefined'){
+          manifest[this.selectedFeed.url].color = this.selectedFeed.color;
+
+          this.ibex.saveManifest(manifest)
+          .then(res=>{
+            this.hideEditModal();
+            this.messages.push({
+              id:"e"+new Date().valueOf(),
+              title:"Feed Updated!",
+              message:"Feed update successfully",
+              color:"primary"
+            });
+            this.getPublicFeeds();
+            this.isSaving = false;
+          })
+          .catch(err=>{
+            console.error(err);
+            this.messages.push({
+              id:"e"+new Date().valueOf(),
+              title:"Error while updating feed",
+              message:err,
+              color:"red"
+            });
+            this.isSaving = false;
+          })
+
+        }
+      })
+      .catch(err=>{
+        console.error(err);
+      })
+    },
+
+    dismissMessage(message){
+      //console.info(id)
+      this.messages.splice(message);
     },
   },
   async created() {
